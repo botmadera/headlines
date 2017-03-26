f5611e859dc0904b7dd713c658b1697021a4b64e
botmadera <botmadera@gmail.com>
2017-03-26 17:40:52 -0400

    add currency

--- headlines.py
+++ headlines.py
@@ -14,9 +14,13 @@ RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
 	    'iol':'http://www.iol.co.za/cmlink/1.640'}
 
 DEFAULTS = {'publication':'bbc',
-             'city':'Asuncion,PY'}
+             'city':'Asuncion,PY',
+             'currency_from':'GBP',
+             'currency_to':'USD'     
+           }
 
 WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=ca9fc36ec5a528f44fdd64d03ba422a3"
+CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=42a70c11cb134ddea6999e5325c66f21"
 
 # WEBSITE ROOT
 @app.route("/")
@@ -35,9 +39,26 @@ def home():
     
     weather = get_weather(city)
 
-    return render_template("home.html", articles = articles, weather = weather)
+    # get customized currency based on user input or default
+    currency_from = request.args.get("currency_from")
+    if not currency_from:
+        currency_from = DEFAULTS['currency_from']
+    
+    currency_to = request.args.get("currency_to")
+    if not currency_to:
+        currency_to = DEFAULTS['currency_to']
+
+    rate, currencies = get_rate(currency_from, currency_to)
+    
+    return render_template("home.html", articles=articles,weather=weather,
+                                        currency_from=currency_from, 
+                                        currency_to=currency_to, 
+                                        rate=rate,
+                                        currencies=sorted(currencies))
 
 # FUNCTIONS
+
+#RSS_FEEDS
 def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
         publication = "bbc"
@@ -47,6 +68,7 @@ def get_news(query):
     feed = feedparser.parse(RSS_FEEDS[publication])
     return feed['entries']
 
+#OPENWEATHERMAP
 def get_weather(query):
     query = urllib.quote(query)
     url = WEATHER_URL.format(query)
@@ -60,6 +82,16 @@ def get_weather(query):
                     'country': parsed['sys']['country']
                   }
     return weather
+
+# OPENEXCHANGERATES
+def get_rate(frm, to):
+    all_currency = urllib2.urlopen(CURRENCY_URL).read()
+    
+    parsed = json.loads(all_currency).get('rates')
+    frm_rate = parsed.get(frm.upper())
+    to_rate = parsed.get(to.upper())
+    
+    return (to_rate/frm_rate, parsed.keys())
 
 
 
